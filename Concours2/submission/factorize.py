44590d25a3f456296a13a47d32b7fffec3fe1ea8
gezhaoqi <gezhaoqi@Titanes-MacBook-Pro.local>
2020-11-08 16:45:02 +0100

    C2 - full storage

--- Concours2/submission/factorize.py
+++ Concours2/submission/factorize.py
@@ -1,84 +1,36 @@
 import sys, os
 import numpy as np
-from random import randint
 
-def quickMulMod(a,b,m):
-    '''a*b%m,  quick'''
-    ret = 0
-    while b:
-        if b&1:
-            ret = (a+ret)%m
-        b//=2
-        a = (a+a)%m
-    return ret
-
-def quickPowMod(a,b,m):
-    '''a^b %m, quick,  O(logn)'''
-    ret =1
-    while b:
-        if b&1:
-            ret = quickMulMod(ret,a,m)
-        b//=2
-        a = quickMulMod(a,a,m)
-    return ret
-
-def isPrime(n, t=5):
-    '''miller rabin primality test, probability result'''
-    t = min(n-3,t) #number of iteration
-    if n < 2 :
+pri = []
+MX = int(1e8)
+isprime = [True] * MX
+ifinit = False
+def init():
+    global MX
+    global ifinit
+    if ifinit:
         return
-    if n == 2 : 
-        return True
-    d = n-1
-    r = 0
-    while d%2 == 0:
-        r += 1
-        d //= 2
-    tested = set()
-    for _ in range(t):
-        a = randint(2, n-2)
-        while a in tested:
-            a = randint(2, n-2)
-        tested.add(a)
-        x = quickPowMod(a,d,n)
-        if x == 1 or x == n-1 : 
-            continue  #success,
-        for _ in range(r-1):
-            x = quickMulMod(x,x,n)
-            if x == n-1 : 
-                break
-        else:
-            return False
-    return True
+    for i in range(2, MX):
+        if isprime[i]:
+            pri.append(i)
+            for j in range(i + i, MX, i):
+                isprime[j] = False
+    ifinit = True
 
 
-def gcd(a,b):
-    while b != 0 :
-        a,b = b,a%b
-    return a
+def factorization(n):
+    global pri
+    ret = []
+    for i in pri:
+        if i * i > n:
+            break
+        while n % i == 0:
+            ret.append(i)
+            n //= i
+    if n != 1:
+        ret.append(n)
+    return ret
 
-def factor(n):
-    '''pollard's rho algorithm'''
-    if n == 1: 
-        return []
-    if isPrime(n):
-        return [n]
-    fact = 1
-    cycle_size = 2
-    x = x_fixed = 2
-    c = randint(1,n)
-    while fact == 1:
-        for _ in range(cycle_size):
-            if fact>1:
-                break
-            x = ( x * x + c ) % n
-            if x == x_fixed:
-                c = randint(1,n)
-                continue
-            fact = gcd(x-x_fixed,n)
-        cycle_size *= 2
-        x_fixed = x
-    return factor(fact) + factor(n//fact)
 
 def factorize(numbers):
     """
@@ -95,14 +47,10 @@ def factorize(numbers):
             -- Ne changez pas le nom de cette fonction, vous pouvez ajouter d'autres fonctions appelées depuis celle-ci.
             -- Ne laissez pas trainer du code hors fonctions car ce module sera importé et du coup un tel code sera exécuté et cela vous pénalisera en temps.
     """
+    init()
     result = {}
     for n in numbers:
-        num = n
-        res = []
-        res = factor(num)
-        #add in dectionary
-        result[n] = res
-
+        result[n] = factorization(n)
     return result # ceci n'est pas une bonne réponse
 
 """
